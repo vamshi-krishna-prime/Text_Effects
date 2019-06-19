40b96c5620d304ba352765b13cb170e9102dd243
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-19 22:49:06 +0530

    Update text_blink.py

--- text_blink.py
+++ text_blink.py
@@ -5,7 +5,7 @@ class text():
     def blink(self, string, num):
         self.blank_list = []
 
-#         breaks and append the text for effect
+#         generates a blank text of same length
         for letter in message:
             self.blank_list.append(" ")
             self.blank_string = "".join(self.blank_list)
