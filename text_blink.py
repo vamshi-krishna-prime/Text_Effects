7a68b306918b6dc07ef5f37299af5bf69bf352f7
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-19 22:44:00 +0530

    Update text_blink.py

--- text_blink.py
+++ text_blink.py
@@ -1,13 +1,23 @@
 import time
 
-def blink(string1, string2):
-    for _ in range(10):
-        print(string1, end='', flush=True)
-        time.sleep(0.1)
-        print('\b\b\b\b\b\b\b\b\b\b\b\b', end='', flush=True)
-        print(string2, end='', flush=True)
-        time.sleep(0.1)
-        print('\b\b\b\b\b\b\b\b\b\b\b\b', end='', flush=True)
-    print(string1)
+class text():
+    def blink(string1, string2, num):
+        for _ in range(num):
+            print(string1, end='', flush=True)
+            time.sleep(0.1)
+            print('\b\b\b\b\b\b\b\b\b\b\b\b', end='', flush=True)
+            print(string2, end='', flush=True)
+            time.sleep(0.1)
+            print('\b\b\b\b\b\b\b\b\b\b\b\b', end='', flush=True)
+        print(string1)
+
+
 if __name__ == '__main__':
-    blink(" Game start ", "            ")
+    message = input("Enter the text > ")
+    blank_list = []
+    for letter in message:
+        blank_list.append(" ")
+        blank_string = "".join(blank_list)
+
+    num = int(input("No.of times to blink > "))
+    fido = text.blink(message, blank_string, num)
