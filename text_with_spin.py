7b99c6dd674117eb3c2f7f7b515ca5dea81245a2
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-20 23:18:22 +0530

    Update text_with_spin.py

--- text_with_spin.py
+++ text_with_spin.py
@@ -1,14 +1,21 @@
 import time
 
-
-def spin(string):
-    for _ in range(10):
-        for ch in '-\\|/':
-            print(ch + ch + string + ch + ch, end='', flush=True)
-            time.sleep(0.1)
-            print('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b', end='', flush=True)
+class text():
+    def spin(self, string, num):
+        self.clear = "\b"*(4 + len(string))
+        for _ in range(num):
+            for ch in '-\\|/':
+                print(ch + ch + string + ch + ch, end='', flush=True)
+                time.sleep(0.1)
+                print(self.clear, end='', flush=True)
+                # example of print statement for string = " Game start "
+                # print('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b', end='', flush=True)
 
 
 if __name__ == '__main__':
-    spin(" Game start ")
-    # print(f"{spin()} Game start {spin()}")
+    string = input("Enter the text > ")
+    num = int(input("No.of times to spin > "))
+    # fido = text.spin(string, num) also works
+    
+    fido = text()
+    fido.spin(string, num)
