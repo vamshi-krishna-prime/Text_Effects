687bc1ae0085158a709040c616a36d52bee63e0e
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-20 23:16:25 +0530

    Update text_with_spin.py

--- text_with_spin.py
+++ text_with_spin.py
@@ -1,19 +1,13 @@
 import time
 
-def spin(string):
+def spin(string1, string2):
     for _ in range(10):
-        for ch in '-\\|/':
-            # print(f"{ch} { string } {ch}", end='', flush=True)
-            print(ch, end='', flush=True)
-            time.sleep(0.1)
-            print('\b', end='', flush=True)
-            print(string, end='', flush=True)
-            time.sleep(0.1)
-            print('\b\b\b\b\b\b\b\b\b\b\b', end='', flush=True)
-            print(f"           {ch}", end='', flush=True)
-            time.sleep(0.1)
-            print('\b\b\b\b\b\b\b\b\b\b\b\b', end='', flush=True)
+        print(string1, end='', flush=True)
+        time.sleep(0.1)
+        print('\b\b\b\b\b\b\b\b\b\b\b\b', end='', flush=True)
+        print(string2, end='', flush=True)
+        time.sleep(0.1)
+        print('\b\b\b\b\b\b\b\b\b\b\b\b', end='', flush=True)
 
 if __name__ == '__main__':
-    spin(" Game start")
-    # print(f"{spin()} Game start {spin()}")
+    spin(" Game start ", "            ")
